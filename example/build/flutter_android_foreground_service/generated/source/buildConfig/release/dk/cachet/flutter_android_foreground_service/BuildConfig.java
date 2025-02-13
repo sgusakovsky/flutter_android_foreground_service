906e7ad1b44d21f525653debae6d436af913f85c
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2025-02-13 13:24:18 +0300

    removed gitignore

---
+++ example/build/flutter_android_foreground_service/generated/source/buildConfig/release/dk/cachet/flutter_android_foreground_service/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dk.cachet.flutter_android_foreground_service;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "dk.cachet.flutter_android_foreground_service";
+  public static final String BUILD_TYPE = "release";
+}
