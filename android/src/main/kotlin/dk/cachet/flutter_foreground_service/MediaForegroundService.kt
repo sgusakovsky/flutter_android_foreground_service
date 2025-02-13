610185dea4956e6761f37538468116bddf3a298d
Anatoly Kot <akot@qulix.com>
2025-02-13 12:42:24 +0300

    Implement MediaForegroundService

---
+++ android/src/main/kotlin/dk/cachet/flutter_foreground_service/MediaForegroundService.kt
@@ -0,0 +1,58 @@
+package dk.cachet.flutter_foreground_service
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.ServiceCompat
+import dk.cachet.flutter_foreground_service.NotificationUtils.Companion.FOREGROUND_SERVICE_NOTIFICATION_ID
+import dk.cachet.flutter_foreground_service.NotificationUtils.Companion.FOREGROUND_SERVICE_TEXT
+import dk.cachet.flutter_foreground_service.NotificationUtils.Companion.FOREGROUND_SERVICE_TITLE
+
+class MediaForegroundService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        var title: String? = null
+        var text: String? = null
+        intent?.apply {
+            title = getStringExtra(FOREGROUND_SERVICE_TITLE)
+            text = getStringExtra(FOREGROUND_SERVICE_TEXT)
+        }
+        startForegroundService(title, text)
+        return START_STICKY
+    }
+
+    private fun startForegroundService(title: String?, text: String?) {
+        val notification = NotificationUtils(this).getForegroundServiceNotification(title, text)
+        startForeground(FOREGROUND_SERVICE_NOTIFICATION_ID, notification)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ServiceCompat.stopForeground(this, ServiceCompat.STOP_FOREGROUND_REMOVE)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    companion object {
+        fun startService(context: Context, title: String? = null, text: String? = null) {
+            val intent = Intent(context, MediaForegroundService::class.java).apply {
+                putExtra(FOREGROUND_SERVICE_TITLE, title)
+                putExtra(FOREGROUND_SERVICE_TEXT, text)
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(intent)
+            } else {
+                context.startService(intent)
+            }
+        }
+
+        fun stopService(context: Context) {
+            context.stopService(Intent(context, MediaForegroundService::class.java))
+        }
+    }
+}
---
+++ android/src/main/kotlin/dk/cachet/flutter_foreground_service/NotificationUtils.kt
@@ -0,0 +1,64 @@
+package dk.cachet.flutter_foreground_service
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class NotificationUtils(private val context: Context) {
+
+    companion object {
+        const val FOREGROUND_SERVICE_TITLE = "foreground_service_title"
+        const val FOREGROUND_SERVICE_TEXT = "foreground_service_text"
+        const val FOREGROUND_SERVICE_NOTIFICATION_ID = 1
+        const val CHANNEL_ID = "foreground_service_channel"
+    }
+
+    fun getForegroundServiceNotification(title: String?, text: String?): Notification {
+        createNotificationChannel()
+
+        val notificationIntent = Intent(context, MediaForegroundService::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            notificationIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val appIcon = getAppIcon(context, context.packageName)
+
+        return NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(title ?: "Media Service")
+            .setContentText(text ?: "Media Service is running")
+            .setSmallIcon(appIcon)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .build()
+    }
+
+    private fun getAppIcon(context: Context, packageName: String): Int {
+        return try {
+            val applicationInfo = context.packageManager.getApplicationInfo(packageName, 0)
+            applicationInfo.icon
+        } catch (e: Exception) {
+            e.printStackTrace()
+            android.R.drawable.sym_def_app_icon
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = context.getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+}
